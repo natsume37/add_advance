dc3dde6d5210dfc14bc0a02742cfa427cee1bd79
natsume37 <natsume37@outlook.com>
2023-06-09 22:20:41 +0800

    logging模块

--- back_pake/日志配置字典.py
+++ back_pake/日志配置字典.py
@@ -1,5 +1,56 @@
-# coding : utf-8
-#夏目&青一
-# @name:日志配置字典
-# @time: 2023/6/9  20:36
+# 核心就在于CV
+import logging
+import logging.config
 
+standard_format = '[%(asctime)s][%(threadName)s:%(thread)d][task_id:%(name)s][%(filename)s:%(lineno)d]' \
+                  '[%(levelname)s][%(message)s]'  # 其中name为getlogger指定的名字
+
+simple_format = '[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d]%(message)s'
+
+logfile_path = 'a3.log'
+# log配置字典
+LOGGING_DIC = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': standard_format
+        },
+        'simple': {
+            'format': simple_format
+        },
+    },
+    'filters': {},  # 过滤日志
+    'handlers': {
+        # 打印到终端的日志
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',  # 打印到屏幕
+            'formatter': 'simple'
+        },
+        # 打印到文件的日志,收集info及以上的日志
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件
+            'formatter': 'standard',
+            'filename': logfile_path,  # 日志文件
+            'maxBytes': 1024 * 1024 * 5,  # 日志大小 5M
+            'backupCount': 5,
+            'encoding': 'utf-8',  # 日志文件的编码，再也不用担心中文log乱码了
+        },
+    },
+    'loggers': {
+        # logging.getLogger(__name__)拿到的logger配置  空字符串作为键 能够兼容所有的日志
+        '': {
+            'handlers': ['default', 'console'],  # 这里把上面定义的两个handler都加上，即log数据既写入文件又打印到屏幕
+            'level': 'DEBUG',
+            'propagate': True,  # 向上（更高level的logger）传递
+        },  # 当键不存在的情况下 (key设为空字符串)默认都会使用该k:v配置
+    },
+}
+
+# 使用配置字典
+if __name__ == '__main__':
+    logging.config.dictConfig(LOGGING_DIC)  # 自动加载字典中的配置
+    logger1 = logging.getLogger('xxx')
+    logger1.debug('好好的 不要浮躁 努力就有收获')
